142b389da6bb556f4fbb5812b399d403c173e58f
medilies <medilies@gmail.com>
2022-11-29 21:35:49 +0100

    seed more users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(15)->create();
 
         \App\Models\User::factory()->create([
             'name' => 'medilies',
             'email' => 'y@y.y',
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'dummy',
+            'email' => 'e@e.e',
         ]);
     }
 }
--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col h-screen bg-gray-900">
-        <nav class="h-16 bg-slate-900 border border-gray-600"></nav>
+        <nav class="h-16 shrink-0 bg-slate-900 border border-gray-600"></nav>
 
         <div class="flex-1">
             <router-view />
