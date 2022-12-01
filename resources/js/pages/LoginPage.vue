db5a1f35bd9df8a96ac5c53f424d11762d5537d0
medilies <medilies@gmail.com>
2022-12-01 21:33:46 +0100

    login form

--- resources/js/pages/LoginPage.vue
+++ resources/js/pages/LoginPage.vue
@@ -1,28 +1,37 @@
 <template>
-    <div>
-        <form @submit.prevent="login">
-            <div>
-                <label for="email"> Username: </label>
-                <input
-                    v-model="email"
-                    type="text"
-                    name="email"
-                    autocomplete="email"
-                />
-            </div>
+    <div class="grid h-full">
+        <div class="place-self-center p-8 bg-slate-700 rounded-md">
+            <form @submit.prevent="login">
+                <div class="grid grid-cols-2 gap-4 items-center">
+                    <label for="email" class="text-white"> Email </label>
 
-            <div>
-                <label for="password"> Password: </label>
-                <input
-                    v-model="password"
-                    type="password"
-                    name="password"
-                    autocomplete="current-password"
-                />
-            </div>
+                    <input
+                        v-model="email"
+                        type="text"
+                        name="email"
+                        autocomplete="email"
+                        class="rounded-md"
+                    />
 
-            <button type="submit">Login</button>
-        </form>
+                    <label for="password" class="text-white"> Password </label>
+
+                    <input
+                        v-model="password"
+                        type="password"
+                        name="password"
+                        autocomplete="current-password"
+                        class="rounded-md"
+                    />
+
+                    <button
+                        type="submit"
+                        class="col-span-2 px-8 py-2 bg-slate-800 text-gray-100 rounded-md hover:bg-slate-900 hover:text-white"
+                    >
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
