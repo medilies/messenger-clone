c4abb1bf8886b8e0d44b8c228b80f204504315bf
medilies <medilies@gmail.com>
2022-12-04 17:51:57 +0100

    FIX: Router Link Active Class

--- resources/js/modules/chat/Components/UsersListItem.vue
+++ resources/js/modules/chat/Components/UsersListItem.vue
@@ -1,30 +1,25 @@
 <template>
     <RouterLink
         :to="{ name: 'messages.direct', params: { id: props.user.id } }"
-        :class="currentChatUserClass"
-        class="w-full block hover:text-white rounded-md p-1 text-left truncate cursor-pointer"
+        class="w-full block hover:text-white rounded-md p-1 text-left truncate cursor-pointer text-blue-300 bg-blue-900"
     >
         {{ user.name }}
     </RouterLink>
 </template>
 
 <script setup>
-import { reactive } from "vue";
-import { useRoute } from "vue-router";
-
 const props = defineProps({
     user: {
         type: Object,
         required: true,
     },
 });
-
-const route = useRoute();
-
-const currentChatUserClass = reactive([
-    // ! not reactive check
-    props.user.id === parseInt(route.params.id)
-        ? "text-white bg-blue-800"
-        : "text-blue-300 bg-blue-900",
-]);
 </script>
+
+<style lang="css" scoped>
+@import "tailwindcss/components";
+
+.router-link-active {
+    @apply text-white bg-blue-800;
+}
+</style>
