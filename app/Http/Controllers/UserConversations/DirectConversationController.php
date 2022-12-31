11d8cd2ecc30fa0257ef0262fc889633bec26b4e
medilies <medilies@gmail.com>
2022-12-31 17:56:26 +0100

    getConversation find or create

--- app/Http/Controllers/UserConversations/DirectConversationController.php
+++ app/Http/Controllers/UserConversations/DirectConversationController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers\UserConversations;
 
 use App\Http\Controllers\Controller;
+use App\Models\Conversation;
 use App\Models\User;
 use App\Services\Conversations\CreateConversationService;
 
 class DirectConversationController extends Controller
 {
-    public function getConversation(CreateConversationService $createConversationService, User $user)
+    public function getConversation(CreateConversationService $create_conversation_service, User $user)
     {
-        return $createConversationService->create(auth()->user(), $user);
+        $conversation_name = $create_conversation_service->formatDirectConversationName(auth()->user(), $user);
+
+        /** @var Conversation */
+        $conversation = Conversation::where('name', $conversation_name)->first();
+
+        if ($conversation) {
+            return $conversation->load('users');
+        }
+
+        return $create_conversation_service->create(auth()->user(), $user);
     }
 }
