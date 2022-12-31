3c8d221d1d3beb053d54b597d0867658d629f413
medilies <medilies@gmail.com>
2022-12-31 19:16:34 +0100

    Improve/Fix find create DirectConversation

--- app/Http/Controllers/UserConversations/DirectConversationController.php
+++ app/Http/Controllers/UserConversations/DirectConversationController.php
@@ -5,21 +5,13 @@ namespace App\Http\Controllers\UserConversations;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Repositories\ConversationRepository;
-use App\Services\Conversations\CreateConversationService;
 
 class DirectConversationController extends Controller
 {
     public function getConversation(
         ConversationRepository $conversation_repository,
-        CreateConversationService $create_conversation_service,
         User $user
     ) {
-        $conversation = $conversation_repository->findDirectConversation(auth()->user(), $user);
-
-        if ($conversation) {
-            return $conversation->load('users');
-        }
-
-        return $create_conversation_service->create(auth()->user(), $user);
+        return $conversation_repository->findOrcreateDirectConversation(auth()->user(), $user);
     }
 }
