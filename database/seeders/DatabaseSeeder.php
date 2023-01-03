b4a54ca0efc327bcd585b065cc5e5e8851d06144
medilies <medilies@gmail.com>
2023-01-03 21:42:30 +0100

    direct conversation basic seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Message;
 use App\Models\User;
+use App\Repositories\ConversationRepository;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -25,5 +27,14 @@ class DatabaseSeeder extends Seeder
         $other_users = User::factory(30)->create();
 
         $users_ids = range(1, $other_users->count() + 2);
+
+        /** @var ConversationRepository */
+        $conversation_repo = app()->make(ConversationRepository::class);
+
+        foreach ($other_users as $user) {
+            $conversation = $conversation_repo->createDirectConversation($first_user, $user);
+
+            Message::factory()->for($conversation)->for($user)->count(2)->create();
+        }
     }
 }
